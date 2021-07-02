41b803a144275e010659a52d3ef1f50544889bf3
Adrian Leal <lea19019@byui.edu>
2021-07-02 08:26:00 -0500

    Finished App

--- src/components/Tasks/Task.js
+++ src/components/Tasks/Task.js
@@ -10,34 +10,15 @@ const Task = (props) => {
         setIsCompleted(!isCompleted);
     }
 
-
-    // const deleteTaskHandler = async () => {
-    //     try {
-    //         const key = props.id
-    //         const response = await fetch('http://localhost:5050/delete-task/' + key, {
-    //             method: 'DELETE'
-    //         });
-
-    //         if (!response.ok) {
-    //             throw new Error('Something went wrong!');
-    //         };
-
-    //         const data = await response.json();
-    //         console.log(data);
-
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-
-    // };
-
+    // If user wants to delete the task this will pass the task id and call the delete function.
     const deleteHandler = () => {
         deleteTaskHandler(props.id);
     };
 
-
+    // Asynchronous function to update our task, and with this we complet our CRUD operations.
     const updateTaskHandler = async () => {
         const id = props.id
+        // Save our data in an object.
         const taskData = {
             id: id,
             content: props.content,
@@ -46,14 +27,15 @@ const Task = (props) => {
         };
 
         try {
-            const response = await fetch('http://localhost:5050/edit-task/' + id, {
+            const response = await fetch(`http://localhost:5050/edit-task/${id}`, { // We pass our id in the params
                 method: 'PATCH',
-                body: JSON.stringify(taskData),
+                body: JSON.stringify(taskData), // Stringify our data so it can be read in JSON
                 headers: {
                     'Content-Type': 'application/json'
                 }
             });
 
+            // Same mechanism to check for errors.
             if (!response.ok) {
                 throw new Error('Something went wrong!');
             };
@@ -64,23 +46,26 @@ const Task = (props) => {
         } catch (error) {
             console.log(error);
         }
-
-
-
     };
 
+    let taskDate = props.dateCreation;
+
     let task;
+    // Depending on the view we are, set in TaskList.js file we will display one or the other task view.
+    // The all view will have the ability to delete or update data.
     if (props.isAllView) {
-        task = <div >
+        task = <div className={classes.allView}>
             <input type="checkbox" onClick={updateTaskHandler} onChange={changeCompleteStatus} checked={isCompleted} />
-            <h2>{props.content}</h2>
-            <h3>{props.dateCreation}</h3>
+            <section>
+                <h2>{props.content}</h2>
+                <p>{taskDate}</p>
+            </section>
             <button onClick={deleteHandler}>X</button>
         </div>
     } else {
         task = <div >
             <h2>{props.content}</h2>
-            <h3>{props.dateCreation}</h3>
+            <p>{taskDate}</p>
         </div>
     }
 
--- src/components/Tasks/TaskList.js
+++ src/components/Tasks/TaskList.js
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
 import classes from './TaskList.module.css';
+import Button from '../UI/Button';
 import Task from './Task';
 
 const TaskList = (props) => {
     const [taskView, setTaskView] = useState('all');
 
+    // Depending the button we press we will send a URL to the App.js file to display
+    // the desired view.
     const getCompleteURL = () => {
         setTaskView('complete')
-        props.onChangeTaskURL('http://localhost:5050/completed');
+        props.onChangeTaskURL('http://localhost:5050/completed'); // Completed view
     };
     const getAllURL = () => {
         setTaskView('all')
-        props.onChangeTaskURL('http://localhost:5050/');
+        props.onChangeTaskURL('http://localhost:5050/'); // All view
     };
     const getPendingURL = () => {
         setTaskView('pending')
-        props.onChangeTaskURL('http://localhost:5050/pending');
+        props.onChangeTaskURL('http://localhost:5050/pending'); // Pending view
     };
 
     let taskList;
 
-    if (taskView != 'all') {
+    // Note: we will be able to update or delete the task only on the all view, this is why
+    // we will pass some extra props to the all view. 
+    if (taskView !== 'all') {
         taskList = props.taskData.map((task) => (
             <Task
                 key={task.id}
@@ -44,14 +49,15 @@ const TaskList = (props) => {
         ));
     }
 
+    // Display our task list and our buttons.
     return (
         <div>
-            <ul >
+            <ul className={classes.taskList}>
                 {taskList}
             </ul>
-            <button onClick={getCompleteURL}>Completed</button>
-            <button onClick={getAllURL}>All</button>
-            <button onClick={getPendingURL}>Pending</button>
+            <Button onClick={getCompleteURL}>Completed</Button>
+            <Button onClick={getAllURL}>All</Button>
+            <Button onClick={getPendingURL}>Pending</Button>
         </div>
     );
 };
